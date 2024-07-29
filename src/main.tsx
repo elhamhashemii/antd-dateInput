8fa4e8a30502ca6ef9e309b75d234b4f8d69e0cc
e.hashemi <e.hashemi@samatco.ir>
2024-07-29 16:40:23 +0330

    depolyment router issue fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {
-  RouterProvider,
+  HashRouter,
 } from "react-router-dom";
-import router from './router.tsx';
-// import 'antd/dist/antd.css';
+import { App } from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <HashRouter>
+    <App />
+  </HashRouter>,
 )
